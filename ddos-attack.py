b63e6f2889f5dca662a72a367ffaa9c6e24dc4ec
tjdqls0213 <81665496+tjdqls0213@users.noreply.github.com>
2021-03-31 12:29:13 +0900

    Add files via upload

--- ddos-attack.py
+++ ddos-attack.py
@@ -20,10 +20,8 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Maker   : Kim Story"
+print "Discord : Kim Story#6170"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
